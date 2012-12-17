1c58694cc7da7e73a5790d9ce993acc5ded1d5e2
Philip Collin <lyeeedar@gmail.com>
2012-12-17 12:59:10 +0000

    Shaders changed to support Light baking

--- assets/data/shaders/model/vertex_lighting.fragment.glsl
+++ assets/data/shaders/model/vertex_lighting.fragment.glsl
@@ -11,15 +11,6 @@ precision lowp float;
 uniform sampler2D u_diffuse_texture;
 #endif
 
-#ifdef u_lightmap_textureFlag
-uniform sampler2D u_lightmap_texture;
-#endif
-
-#ifdef u_fog_colourFlag
-uniform vec4 u_fog_colour;
-varying float v_fog;
-#endif
-
 #ifdef u_glowFlag
 uniform float u_glow;
 #endif
@@ -30,21 +21,9 @@ void main()
 {		
 	vec4 light = v_diffuse;
 	
-#ifdef u_lightmap_textureFlag
-	light *= texture2D(u_lightmap_texture, v_texCoords);
-#endif
-	
 #ifdef u_diffuse_textureFlag
 	light *= texture2D(u_diffuse_texture, v_texCoords);
 #endif
-	
-#ifdef u_fog_colourFlag
-	light.rgb = mix(light.rgb, u_fog_colour.rgb, v_fog);		
-		#ifdef u_translucentFlag
-			light.a += v_fog;
-		#endif
-#endif
-	
 
 	gl_FragColor.rgb = light.rgb;
 
--- assets/data/shaders/model/vertex_lighting.vertex.glsl
+++ assets/data/shaders/model/vertex_lighting.vertex.glsl
@@ -1,13 +1,12 @@
 attribute vec3 a_position; 
 attribute vec2 a_texCoord0;
 attribute vec3 a_normal;
+attribute vec3 a_colour;
+attribute vec3 a_baked_light;
 
 uniform mat3 u_normal_matrix;
-uniform vec4 u_cam_position;
-uniform vec3 u_cam_direction;	
 uniform mat4 u_pv;
 uniform mat4 u_model_matrix;
-uniform vec4 u_ambient;
 
 #if LIGHTS_NUM > 0
 uniform vec3 u_light_positions[LIGHTS_NUM];
@@ -16,46 +15,8 @@ uniform float u_light_attenuations[LIGHTS_NUM];
 uniform float u_light_intensities[LIGHTS_NUM];
 #endif
 
-#if DIR_LIGHTS > 0
-uniform vec3 u_directional_light_direction[DIR_LIGHTS];
-uniform vec3 u_directional_light_colour[DIR_LIGHTS];
-#endif
-
-#ifdef u_diffuse_colourFlag
-uniform vec4 u_diffuse_colour;
-#endif
-
-#ifdef u_emissive_colourFlag
-uniform vec4 u_emissive_colour;
-#endif
-
-#ifdef u_rim_colourFlag
-uniform vec4 u_rim_colour;
-#endif
-
-#ifdef u_fog_colourFlag
-varying float v_fog;
-#endif
-
 varying vec2 v_texCoords;
 varying vec4 v_diffuse;
-
-//wrap light. this is fastest light model
-vec3 wrapLight(vec3 l_vector, vec3 l_colour, float l_attenuation, vec3 n_dir){
-
-	if (length(l_colour) == 0) return vec3(0, 0, 0);
-
-	vec3 dif  = l_vector;
-	//fastest way to calculate inverse of length				
-	//float invLen = inversesqrt(dot(dif, dif));
-	float weight = l_attenuation;
-				
-	//vec3 L = invLen * dif;// normalize
-	float lambert = dot(n_dir, l_vector) * 0.5 + 0.5;
-	weight *= lambert;
-
-	return l_colour * weight;
-}
 
 // Diffuse light model. It actually works :p
 vec3 calculateLight(vec3 l_vector, vec3 l_colour, float l_attenuation, float l_intensity, vec3 n_dir)
@@ -81,14 +42,6 @@ void main()
 	vec3 normal = normalize(u_normal_matrix * a_normal);
 	
 	vec3 light_agg_col = vec3(0, 0, 0);
-	vec3 material_agg_col = vec3(0, 0, 0);
-
-#if DIR_LIGHTS > 0
-	for (int i = 0; i < DIR_LIGHTS; i++)
-	{
-		light_agg_col += u_direction_light_colour[i] * wrapLight(normal, -u_directional_light_direction[i]);
-	}
-#endif
 
 #if LIGHTS_NUM > 0		
 	for ( int i = 0; i < LIGHTS_NUM; i++ ){	
@@ -99,33 +52,10 @@ void main()
 	}
 #endif
 
-#ifdef u_diffuse_colourFlag
-	material_agg_col += u_diffuse_colour.rgb;
-	#ifdef translucentFlag
-		v_diffuse.a = u_diffuse_colour.a;	
-	#endif
-#else
-	material_agg_col += vec3(1.0, 1.0, 1.0);
-#endif
-
-#ifdef u_emissive_colourFlag
-	material_agg_col += u_emissive_colour.rgb;
-#endif
-
-#ifdef u_rim_colourFlag
-	material_agg_col.rgb +=  pow( 1.0 - dot( normal, -u_cam_direction ), 2.0 ) * u_rim_colour.rgb;
-#endif
-
-#ifdef u_fog_colourFlag
-	float fog  =  (distance(pos, u_cam_position.xyz) * u_cam_position.w);
-	fog *=fog;	
-	v_fog = min(fog, 1.0);	
-#endif
-
 #if LIGHTS_NUM > 0
-	v_diffuse.rgb = material_agg_col * (u_ambient.rgb + light_agg_col);
+	v_diffuse.rgb = a_colour * (a_baked_light + light_agg_col);
 #else
-	v_diffuse.rgb = material_agg_col;
+	v_diffuse.rgb = a_colour;
 #endif
 	
 }
